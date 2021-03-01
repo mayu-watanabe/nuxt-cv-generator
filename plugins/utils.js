a1af3a04cee51d002d078060da7124ff6a3ae36c
mayu-watanabe <mayu.watanabe.1994@gmail.com>
2021-03-01 15:24:07 +0900

    apply pdf style

--- plugins/utils.js
+++ plugins/utils.js
@@ -6,102 +6,160 @@ pdfMake.vfs = pdfFonts.pdfMake.vfs;
 pdfMake.fonts = {
   GenShinGothic: {
     normal: "GenShinGothic-Normal-Sub.ttf",
-    bold: "GenShinGothic-Normal-Sub.ttf"
+    bold: "GenShinGothic-Normal-Sub.ttf",
+    italics: "GenShinGothic-Normal-Sub.ttf",
+    bolditalics: "GenShinGothic-Normal-Sub.ttf",
   }
 };
 
 const outputPdf = (data) => {
+  var today = getDateToday();
   const html = `
-  <div>
-  <h1>職務経歴書</h1>
-  ${(!data.basic) ? `` :
-      `<h2>基本情報</h2>` +
-      '<b>氏名</b>' +
-      ((data.basic.familyName && data.basic.firstName) ? `<p>${data.basic.familyName} ${data.basic.firstName}</p>` : ``) +
-      ((data.basic.github) ? `<b>GitHub</b><p>${data.basic.github}</p>` : ``) +
-      ((data.basic.website) ? `<b>Website</b><p>${data.basic.website}</p>` : ``)
-    }
-  ${(!data.company.length) ? `` :
-      `<h2>職務経歴</h2>` +
+  <div class="wrapper">
+    <h1 class="title_main">職務経歴書</h1>
+    <span class="date_published">${today}現在</span>
+    ${(!data.basic) ? `` : `
+    <div class="section">
+      <h3 class="title_sub">基本情報</h3>` +
+      '<span class="title_sub_sub">・氏名</span>' +
+      ((data.basic.familyName && data.basic.firstName) ? `<span style="margin:6">${data.basic.familyName} ${data.basic.firstName}</span>` : ``) +
+      ((data.basic.github) ? `<span class="title_sub_sub">・GitHub</span><span style="margin:6">${data.basic.github}</span>` : ``) +
+      ((data.basic.website) ? `<span class="title_sub_sub">・Website</span><span style="margin:6">${data.basic.website}</span>` : ``)
+      }
+    </div><br>
+    ${(!data.company.length) ? `` : `
+    <div class="section">
+      <h3 class="title_sub">職務経歴</h3>` +
       data.company.map(company => `
-      <li>${company.name}<li>${company.summery}</li></li>
-    `).join('') +
-      `<h2>開発経歴</h2>` +
+      <li>・${company.name}</li><span style="margin:6">${company.summery}</span>
+    </div><br>
+    `).join('') + `
+    <div class="section">
+      <h3 class="title_sub">開発経歴</h3>` +
       data.company.map(company => `
       <div>
-      <h3>${company.name} (${company.fromYear}年${company.fromMonth}月~${company.toYear}年${company.toMonth}月)</h3>
-          <p>資本金: ${company.capital}円 従業員数: ${company.employeesNumber}名</p>
-          <p>事業内容: ${company.industry}</p>
-          <table>
-            <thead>
-              <tr>
-                <th>期間</th>
-                <th>業務内容</th>
-                <th>環境</th>
-                <th>役割／担当／規模</th>
-              </tr>
-            </thead>
-            <tbody>
-            ${company.projects.map(project => `
-              <tr>
-                <td>${project.fromYear}.${project.fromMonth}~${project.toYear}.${project.toMonth}</td>
-                <td>
-                  <b>プロジェクト要約</b><p>${project.summery}</p>
-                  <b>担当フェーズ</b><p>${project.phases}</p>
-                  <b>実績・取り組み等</b><p>${project.achievement}</p>
-                </td>
-                <td>${project.environment}</td>
-                <td>
-                  <b>役割</b><p>${project.role}</p>
-                  <b>プロジェクト規模</b><p>${project.numberOfTeam}名</p>
-                </td>
-              </tr>
-            `).join('')}
-            </tbody>
-          </table>
+        <h5>${company.name} (${formatPeriod(company.fromYear, company.fromMonth)}~${formatPeriod(company.toYear, company.toMonth)})</h5>
+        <span>資本金: ${company.capital}円　従業員数: ${company.employeesNumber}名　事業内容: ${company.industry}</span>
+        <br>
+        <table>
+          <thead>
+            <tr>
+              <th style="width:15%">期間</th>
+              <th style="width:50%">業務内容</th>
+              <th style="width:20%">環境</th>
+              <th style="width:15%">役割／規模</th>
+            </tr>
+          </thead>
+          <tbody>
+          ${company.projects.map(project => `
+            <tr>
+              <td style="alignment:center;margin:15">${formatPeriod(project.fromYear, project.fromMonth)}<br>~<br>${formatPeriod(project.toYear, project.toMonth)}</td>
+              <td>
+                <b>【プロジェクト要約】</b><p style="margin:10">${project.summery}</p>
+                <b>【担当フェーズ】</b><p style="margin:10">${project.phases}</p>
+                <b>【実績・取り組み等】</b><p style="margin:10">${project.achievement}</p>
+              </td>
+              <td>${project.environment}</td>
+              <td>
+                <b>【役割】</b><p style="margin:8">${project.role}</p>
+                <b>【規模】</b><p style="margin:8">${project.numberOfTeam}名</p>
+              </td>
+            </tr>
+          `).join('')}
+          </tbody>
+        </table>
       </div>
+    </div><br>
     `).join('')
-  }
-  ${ (!data.skills.length) ? `` :`
-  <h2>プログラミングスキル</h2>
-  <table>
-    <thead>
-      <tr>
-        <th>カテゴリ</th>
-        <th>種別</th>
-        <th>経験年数</th>
-        <th>スキルレベル・備考</th>
-      </tr>
-    </thead>
-    <tbody>` +
-    data.skills.map(skill => `
-      <tr>
-        <td>${skill.categoryName}</td>
-        <td>${skill.name}</td>
-        <td>${skill.period}年</td>
-        <td>${skill.memo}</td>
-      </tr>
-    `).join('') +
-    `</tbody>
-  </table>
-  `}
-  ${ (!data.skills.length) ? `` : `
-  <h2>自己PR</h2>` + 
-    data.summery.map(summery => `
-    <div>
-      <b>${summery.title}</b>
-      <p>${summery.content}</p>
+    }
+    ${ (!data.skills.length) ? `` :`
+    <div class="section">
+      <h3 class="title_sub">プログラミングスキル</h3>
+      <br>
+      <table>
+        <thead>
+          <tr>
+            <th style="width:10%">カテゴリ</th>
+            <th style="width:15%">種別</th>
+            <th style="width:10%">経験年数</th>
+            <th style="width:65%">スキルレベル・備考</th>
+          </tr>
+        </thead>
+        <tbody>` +
+        data.skills.map(skill => `
+          <tr>
+            <td style="alignment:center">${skill.categoryName}</td>
+            <td style="alignment:center">${skill.name}</td>
+            <td style="alignment:center">${skill.period}年</td>
+            <td>${skill.memo}</td>
+          </tr>
+        `).join('') +
+        `</tbody>
+      </table>
+    </div><br>`}
+    ${ (!data.skills.length) ? `` : `
+    <div class="section">
+      <h3 class="title_sub">自己PR</h3>` + 
+        data.summery.map(summery => `
+        <div>
+          <h6 class="title_sub_sub">${summery.title}</h6>
+          <span>${summery.content}</span>
+        </div>
+      `).join('')
+      }
     </div>
-  `).join('')
-  }
-  `;
+  </div>`;
   const docDefinition = { 
+    pageSize: 'A4',
     defaultStyle: {
-      font: "GenShinGothic"
+      font: "GenShinGothic",
+      fontSize: 11,
     },
-    content: [htmlToPdfmake(html)],
-}; 
-  pdfMake.createPdf(docDefinition).open();
+    styles: {
+      'html-span': {
+        fontSize: 11,
+        margin: [2, 3]
+      },
+      'html-table': {
+        fontSize: 11
+      },
+      'html-th': {
+        alignment: "center",
+        fontSize: 10
+      },
+      title_main:{
+        alignment: "center"
+      },
+      title_sub: {
+        margin: [0, 8],
+        fontSize: 18,
+      },
+      title_sub_sub: {
+        margin: [0, 10],
+      },
+      date_published: {
+        alignment: "right",
+      },
+    },
+    content: [htmlToPdfmake(html, {tableAutoSize:true})],
+  }; 
+  pdfMake.createPdf(docDefinition).download();
+}
+
+function getDateToday () {
+  var today = new Date();
+  var year = today.getFullYear();
+  var month = today.getMonth() + 1;
+  var day = today.getDate();
+
+  return `${year}年${month}月${day}日`;
+}
+
+function formatPeriod(year, month) {
+  if (month == '現在') {
+    return month;
+  }
+  return `${year}.${month}`
 }
 
 export default ({}, inject) => {
